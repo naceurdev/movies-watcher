ce50f3fd6341e7b96a96b6b34c34abb64b95e557
Ben Yahia <naceurbenyahia1@gmail.com>
2025-11-03 19:31:08 +0100

    first commit

---
+++ src/user/entities/user.entity.ts
@@ -0,0 +1,22 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
+
+  @Column()
+  userName: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  role: string;
+}
