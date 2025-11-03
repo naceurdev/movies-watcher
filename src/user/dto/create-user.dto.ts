1cc40ecd083790058852d4a51be3f37833295c02
Ben Yahia <naceurbenyahia1@gmail.com>
2025-11-03 22:30:32 +0100

    add user validation

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,14 @@
-export class CreateUserDto {}
+import { IsString } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  firstName: string;
+  @IsString()
+  lastName: string;
+  @IsString()
+  userName: string;
+  @IsString()
+  password: string;
+  @IsString()
+  role: string;
+}
--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,4 +1,16 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
+import { IsString } from 'class-validator';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+  @IsString()
+  firstName: string;
+  @IsString()
+  lastName: string;
+  @IsString()
+  userName: string;
+  @IsString()
+  password: string;
+  @IsString()
+  role: string;
+}
