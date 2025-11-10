258a222190007da8ad3179c929be288fe7e4c999
Ben Yahia <naceurbenyahia1@gmail.com>
2025-11-10 12:37:37 +0100

    import auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { UserModule } from './user/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user/entities/user.entity';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -23,6 +24,7 @@ import { ConfigModule } from '@nestjs/config';
       synchronize: true,
     }),
     UserModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
